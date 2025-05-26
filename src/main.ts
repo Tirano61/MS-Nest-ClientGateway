ef6d4bdfd35ac57fe6b311dfe3fbfff54831e086
Dario Ramirez <dariorama60@gmail.com>
2025-05-25 22:03:34 -0300

    Se agrego console log en main

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
+  console.log('Hola mundo - primer cambio!!');
+
   await app.listen(envs.port);
   logger.log(`Gateway running on port: ${ envs.port}`)
 }
